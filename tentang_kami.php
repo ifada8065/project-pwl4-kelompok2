7695ab35bf0832d95fb6e113b9a3c38450fdee14
ragil8113 <ragil8113@students.amikom.ac.id>
2016-12-21 02:38:06 +0700

    Delete tentang_kami.php

--- tentang_kami.php
+++
@@ -1,8 +0,0 @@
-<section id="center">
-	<div class="container">
-		<div class="cont-text">
-			<h3 class="purple">Tentang Kami</h3>
-          	<p>Isi content tentang kami	</div>
-	<div class="strip-rainbow-rule"></div> 
-	</div>     
-</section>
